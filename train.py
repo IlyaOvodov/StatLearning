83106b337177d654447dc2c20061779b89eba974
Ilya Ovodov <igovodov@sberbank.ru>
2025-04-16 20:47:30 +0300

    Remove AMP for simplicity

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import numpy as np
 from pathlib import Path
 import torch
-from torch.cuda.amp import GradScaler, autocast
 from torch.nn import CrossEntropyLoss
 from torch.optim import SGD, lr_scheduler
 from torch.utils.tensorboard import SummaryWriter
@@ -14,15 +13,14 @@ import loaders
 BATCH_SIZE = 512
 BASE_LR = 0.1
 MOMENTUM = 0.9
-USE_AMP = False
 BASE_LOG_DIR = Path(__file__).parent / 'results/statopt'
-EXPERIMENT = f'baseline_bs{BATCH_SIZE}_lr{BASE_LR}_m{MOMENTUM}_AMP{USE_AMP}'
+EXPERIMENT = f'baseline_bs{BATCH_SIZE}_lr{BASE_LR}_m{MOMENTUM}'
 
 EPOCHS = 24
 VAL_STEP = 50000
 USE_TTA_EVAL = False
 
-train_loader, test_loader = loaders.create_cifar_loaders(BATCH_SIZE, use_amp=USE_AMP)
+train_loader, test_loader = loaders.create_cifar_loaders(BATCH_SIZE, use_amp=False)
 model = model.create_model()
 
 def train():
@@ -32,7 +30,6 @@ def train():
                             [0, 5 * iters_per_epoch, EPOCHS * iters_per_epoch],
                             [0, 1, 0])
     scheduler = lr_scheduler.LambdaLR(opt, lr_schedule.__getitem__)
-    scaler = GradScaler(enabled=USE_AMP)
     loss_fn = CrossEntropyLoss(label_smoothing=0.1)
     
     writer = SummaryWriter(log_dir=BASE_LOG_DIR / EXPERIMENT)
@@ -44,12 +41,10 @@ def train():
         for ims, labs in pbar:
             global_step += BATCH_SIZE
             opt.zero_grad(set_to_none=True)
-            with autocast(enabled=USE_AMP):
-                out = model(ims)
-                loss = loss_fn(out, labs)
-            scaler.scale(loss).backward()
-            scaler.step(opt)
-            scaler.update()
+            out = model(ims)
+            loss = loss_fn(out, labs)
+            loss.backward()
+            opt.step()
             scheduler.step()
             writer.add_scalar('train/loss', loss.mean().item(), global_step)
             writer.add_scalar('train/lr', opt.param_groups[0]['lr'], global_step)
@@ -58,8 +53,6 @@ def train():
             eval(writer, global_step)
             prev_eval_step = global_step
 
-
-
 def eval(writer, global_step):
     model.eval()
     metrix = dict()
@@ -67,15 +60,14 @@ def eval(writer, global_step):
         total_correct, total_correct_flip, total_num = 0., 0., 0.
         pbar = tqdm(test_loader)
         for ims, labs in pbar:
-            with autocast():
-                total_num += ims.shape[0]
-                out = model(ims)
-                total_correct += out.argmax(1).eq(labs).sum().cpu().item()
-                metrix['accuracy'] = total_correct / total_num * 100
-                if USE_TTA_EVAL:
-                    out_flip = (out + model(torch.fliplr(ims))) / 2. # Test-time augmentation
-                    total_correct_flip += out_flip.argmax(1).eq(labs).sum().cpu().item()
-                    metrix['accuracy with TTA'] = total_correct_flip / total_num * 100
+            total_num += ims.shape[0]
+            out = model(ims)
+            total_correct += out.argmax(1).eq(labs).sum().cpu().item()
+            metrix['accuracy'] = total_correct / total_num * 100
+            if USE_TTA_EVAL:
+                out_flip = (out + model(torch.fliplr(ims))) / 2. # Test-time augmentation
+                total_correct_flip += out_flip.argmax(1).eq(labs).sum().cpu().item()
+                metrix['accuracy with TTA'] = total_correct_flip / total_num * 100
             pbar.set_postfix(metrix)
     model.train()
     for k, v in metrix.items():
